8f036296accf3513717a391f0bc0f0a7f7b0c5bc
Shivam <shivamchawla678@gmail.com>
2026-04-22 16:27:29 +0530

    Made changes for authcontext

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Servocci from "../assets/Servocci.png";
-import { useAuth } from "../context/authContext";
+import { useAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 import { Eye, EyeOff } from "lucide-react";
 
